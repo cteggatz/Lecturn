6820d8ccbb3ca0ce598a990c4ad6ea835f9cbc01
cteggatz <categgatzjr@gmail.com>
2023-06-07 12:41:48 -0500

    first commit. Working discord bot!

---
+++ src/commands/random/server.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("server")
+        .setDescription("returns information about the server"),
+
+    async execute(interaction){
+        await interaction.reply(
+            `this server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members`
+        )
+    }
+}
